7559f20bb63b1e7d441f0ba6625e9db58968dffe
trown <trown@redhat.com>
2019-07-02 08:25:40 -0400

    Don't set hosts entry on bootstrap

--- data/data/openstack/bootstrap/main.tf
+++ data/data/openstack/bootstrap/main.tf
@@ -61,7 +61,6 @@ data "ignition_file" "hosts" {
     content = <<EOF
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${var.bootstrap_ip} api-int.${var.cluster_domain} api.${var.cluster_domain}
 EOF
   }
 }
